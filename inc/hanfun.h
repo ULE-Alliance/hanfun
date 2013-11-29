3f44419855bc5dddff3a4e6ae288c90e476333e3
Filipe Alves <filipe.alves@bithium.com>
2013-11-29 17:24:05 +0000

    Initial CMake support.

---
+++ inc/hanfun.h
@@ -0,0 +1,25 @@
+// =============================================================================
+/*!
+ * \file	inc/hanfun.h
+ *
+ * This is the top level include file for the HAN-FUN library.
+ *
+ * \if DEV
+ *
+ * \author 	Filipe Alves <filipe.alves@bithium.com>
+ *
+ * \version	x.x.x
+ *
+ * \copyright Copyright &copy; &nbsp; 2012 Bithium S.A.
+ *
+ * \endif
+ */
+// =============================================================================
+
+#ifndef HANFUN_H
+#define HANFUN_H
+
+#include "hanfun/version.h"
+#include "hanfun/config.h"
+
+#endif /* HANFUN_H */
