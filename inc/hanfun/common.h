70079570ee11fc31df25c2c14b1b6caa36d9ac5f
Filipe Alves <filipe.alves@bithium.com>
2013-12-19 16:18:20 +0000

    Added version constants for Core, Profiles and Interfaces.

--- inc/hanfun/common.h
+++ inc/hanfun/common.h
@@ -40,6 +40,10 @@ using namespace std;
  */
 namespace HF
 {
+   constexpr uint8_t CORE_VERSION       = 0;
+   constexpr uint8_t PROFILES_VERSION   = 0;
+   constexpr uint8_t INTERFACES_VERSION = 0;
+
    /*!
     * This class represents a byte array.
     *
