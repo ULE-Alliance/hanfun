b42380cff427e1cb7b4682b9fe6a5bd9b9312fac
Filipe Alves <filipe.alves@bithium.com>
2014-06-20 10:23:47 +0100

    Make memory leak detection work with STL classes.

---
+++ tests/helpers/memory.h
@@ -0,0 +1,22 @@
+// =============================================================================
+/*!
+ * \file       tests/helpers/memory.h
+ *
+ * This file is used to make the used STL classes work with CppUTest memory leak
+ * functionality.
+ *
+ * \author     Filipe Alves <filipe.alves@bithium.com>
+ *
+ * \version    x.x.x
+ *
+ * \copyright  Copyright &copy; &nbsp; 2014 Bithium S.A.
+ */
+// =============================================================================
+
+#ifndef HF_MEMORY_HELPER_H
+#define HF_MEMORY_HELPER_H
+
+#include <forward_list>
+#include "CppUTest/MemoryLeakDetectorMallocMacros.h"
+
+#endif /* HF_MEMORY_HELPER_H */
