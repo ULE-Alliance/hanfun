baacdf1951f055572f124ad8af0c95108dde7417
Filipe Alves <filipe.alves@bithium.com>
2014-07-10 10:43:16 +0100

    Moved debug support to separated include file.

--- apps/application.h
+++ apps/application.h
@@ -17,58 +17,7 @@
 
 #include "hanfun.h"
 
-// =============================================================================
-// Defines
-// =============================================================================
-
-// Debug Macros.
-
-#define NL                   std::endl
-
-#define HF_LOG_LEVEL_NONE    0
-#define HF_LOG_LEVEL_ERROR   1
-#define HF_LOG_LEVEL_WARN    2
-#define HF_LOG_LEVEL_INFO    3
-#define HF_LOG_LEVEL_DEBUG   4
-#define HF_LOG_LEVEL_TRACE   5
-
-#ifndef HF_LOG_LEVEL
-   #define HF_LOG_LEVEL      HF_LOG_LEVEL_INFO
-#endif
-
-#if HF_LOG_LEVEL >= HF_LOG_LEVEL_ERROR
-   #define ERROR             std::cout.clear(); std::cerr << "[ERROR] "
-#else
-   #define ERROR             std::cerr.setstate (std::ios_base::badbit);std::cerr
-#endif
-
-#if HF_LOG_LEVEL >= HF_LOG_LEVEL_WARN
-   #define WARN              std::cout.clear(); std::cerr << "[WARN ] "
-#else
-   #define WARN              std::cerr.setstate (std::ios_base::badbit);std::cerr
-#endif
-
-#if HF_LOG_LEVEL >= HF_LOG_LEVEL_INFO
-   #define INFO              std::cout.clear(); std::cout << "[INFO ] "
-#else
-   #define INFO              std::cout.setstate (std::ios_base::badbit);std::cout
-#endif
-
-#if HF_LOG_LEVEL >= HF_LOG_LEVEL_DEBUG
-   #define DEBUG             std::cout.clear(); std::cout << "[DEBUG] "
-#else
-   #define DEBUG             std::cout.setstate (std::ios_base::badbit);std::cout
-#endif
-
-#if HF_LOG_LEVEL >= HF_LOG_LEVEL_TRACE
-   #define TRACE             std::cout.clear(); std::cout << "[TRACE] "
-#else
-   #define TRACE             std::cout.setstate (std::ios_base::badbit);std::cout
-#endif
-
-#define APP                  std::cout.clear(); std::cout
-
-#define LOG(X)   X
+#include "hanfun/debug.h"
 
 // =============================================================================
 // API
