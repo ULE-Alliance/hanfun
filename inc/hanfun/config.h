508efd4491e0013911edab0cc76651fd9c20d08d
Filipe Alves <filipe.alves@bithium.com>
2014-07-25 13:21:26 +0100

    Adding configuration and version generated files.
    For Dialog, that does not use the CMake build system, we copy over the
    generated files for configuration and software version.

---
+++ inc/hanfun/config.h
@@ -0,0 +1,36 @@
+#ifndef HF_CONFIG_H
+#define HF_CONFIG_H
+
+// =============================================================================
+// Common
+// =============================================================================
+
+#define OFF	0
+#define ON	1
+
+// =============================================================================
+// SimplePowerMeter Configuration
+// =============================================================================
+
+// In testing mode support all attributes.
+
+#define HF_ITF_SPM_ENERGY_ATTR               OFF
+#define HF_ITF_SPM_POWER_ATTR                OFF
+#define HF_ITF_SPM_ENERGY_AT_RESET_ATTR      OFF
+#define HF_ITF_SPM_TIME_AT_RESET_ATTR        OFF
+#define HF_ITF_SPM_AVG_POWER_ATTR            OFF
+#define HF_ITF_SPM_AVG_POWER_INTERVAL_ATTR   OFF
+#define HF_ITF_SPM_VOLTAGE_ATTR              OFF
+#define HF_ITF_SPM_CURRENT_ATTR              OFF
+#define HF_ITF_SPM_FREQUENCY_ATTR            OFF
+#define HF_ITF_SPM_POWER_FACTOR_ATTR         OFF
+#define HF_ITF_SPM_REPORT_INTERVAL_ATTR      OFF
+
+// In testing mode support all commands.
+
+#define HF_ITF_SPM_REPORT_CMD                OFF
+#define HF_ITF_SPM_RESET_CMD                 OFF
+
+// =============================================================================
+
+#endif  /* HF_CONFIG_H */
---
+++ inc/hanfun/version.h
@@ -0,0 +1,10 @@
+#ifndef HF_VERSION_H
+#define HF_VERSION_H
+
+#define HF_MAJOR_VERSION 0
+#define HF_MINOR_VERSION 3
+#define HF_PATCH_VERSION 1
+
+#define HF_VERSION       "0.3.1"
+
+#endif /* HF_VERSION_H */
