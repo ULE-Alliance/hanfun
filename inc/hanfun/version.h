2b7fe88d1d5d5ad0bfb73823a3dc88a925ffe0a7
Filipe Alves <filipe.alves@bithium.com>
2014-07-30 10:07:54 +0100

    Update version header file.

--- inc/hanfun/version.h
+++ inc/hanfun/version.h
@@ -3,8 +3,8 @@
 
 #define HF_MAJOR_VERSION 0
 #define HF_MINOR_VERSION 3
-#define HF_PATCH_VERSION 1
+#define HF_PATCH_VERSION 2
 
-#define HF_VERSION       "0.3.1"
+#define HF_VERSION       "0.3.2"
 
 #endif /* HF_VERSION_H */
