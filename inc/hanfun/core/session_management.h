56d8b537d11c077ec02849a0a3c16387eb800604
Filipe Alves <filipe.alves@bithium.com>
2014-10-17 16:47:19 +0100

    Initial implementation for session management - server side.

---
+++ inc/hanfun/core/session_management.h
@@ -0,0 +1,443 @@
+// =============================================================================
+/*!
+ * \file       inc/hanfun/core/session_manager.h
+ *
+ * This file contains the definitions for the session management functionality.
+ *
+ * \version    x.x.x
+ *
+ * \copyright  Copyright &copy; &nbsp; 2014 ULE Alliance
+ *
+ * For licensing information, please see the file 'LICENSE' in the root folder.
+ *
+ * Initial development by Bithium S.A. [http://www.bithium.com]
+ */
+// =============================================================================
+#ifndef HF_SESSION_MANAGEMENT_H
+#define HF_SESSION_MANAGEMENT_H
+
+#include <algorithm>
+
+#include "hanfun/common.h"
+#include "hanfun/protocol.h"
+
+namespace HF
+{
+   namespace Core
+   {
+      /*! Top level namespace for a the Session Management functionality. */
+      namespace SessionManagement
+      {
+         typedef enum _CMD
+         {
+            START,   //!< Start a new session for a device.
+            GET,     //!< Retrieve information.
+            END,     //!> End the session for device.
+         } CMD;
+
+         // =============================================================================
+         // Messages API
+         // =============================================================================
+
+         /*!
+          * Start Read Session Command Message.
+          */
+         struct StartResponse: public Protocol::Response
+         {
+            uint16_t count; //!< Number of device entries.
+
+            StartResponse (uint16_t count = 0) :
+                  count(count)
+            {
+            }
+
+            //! \see HF::Protocol::Response::size.
+            size_t size () const
+            {
+               return Protocol::Response::size() + sizeof(count);
+            }
+
+            //! \see HF::Protocol::Response::pack.
+            size_t pack (Common::ByteArray &array, size_t offset = 0) const
+                         {
+               size_t start = offset;
+
+               offset += Protocol::Response::pack(array, offset);
+               offset += array.write(offset, count);
+
+               return offset - start;
+            }
+
+            //! \see HF::Protocol::Response::unpack.
+            size_t unpack (const Common::ByteArray &array, size_t offset = 0)
+            {
+               size_t start = offset;
+
+               offset += Protocol::Response::unpack(array, offset);
+               offset += array.read(offset, count);
+
+               return offset - start;
+            }
+         };
+
+         /*!
+          * Get Entries Command Message.
+          */
+         struct GetEntriesMessage
+         {
+            uint16_t offset; //!< Start index for the first entry to be provided.
+            uint8_t count;  //!< Number of entries to be sent in the response.
+
+            constexpr static size_t min_size = sizeof(offset) + sizeof(count);
+
+            GetEntriesMessage (uint16_t offset = 0, uint8_t count = 0) :
+                  offset(offset), count(count)
+            {
+            }
+
+            //! \see HF::Serializable::size.
+            size_t size () const
+            {
+               return min_size;
+            }
+
+            //! \see HF::Serializable::pack.
+            size_t pack (Common::ByteArray &array, size_t offset = 0) const
+            {
+               size_t start = offset;
+
+               offset += array.write(offset, this->offset);
+               offset += array.write(offset, this->count);
+
+               return offset - start;
+            }
+
+            //! \see HF::Serializable::unpack.
+            size_t unpack (const Common::ByteArray &array, size_t offset = 0)
+            {
+               size_t start = offset;
+
+               offset += array.read(offset, this->offset);
+               offset += array.read(offset, this->count);
+
+               return offset - start;
+            }
+         };
+
+         template<typename _Entry>
+         struct GetEntriesResponse: public Protocol::Response
+         {
+            std::vector<_Entry> entries;
+
+            //! \see HF::Protocol::Response::size.
+            size_t size () const
+            {
+               size_t result = Protocol::Response::size() + sizeof(uint8_t);
+
+               std::for_each(entries.begin(), entries.end(), [&result](const _Entry &entry)
+               {
+                  result += entry.size();
+               });
+
+               return result;
+            }
+
+            //! \see HF::Protocol::Response::pack.
+            size_t pack (Common::ByteArray &array, size_t offset = 0) const
+            {
+               size_t start = offset;
+
+               offset += Protocol::Response::pack(array, offset);
+
+               uint8_t count = entries.size();
+               offset += array.write(offset, count);
+
+               /* *INDENT-OFF* */
+               std::for_each(entries.begin(), entries.end(),
+                             [&offset, &array](const _Entry &entry)
+                             {
+                                offset += entry.pack (array, offset);
+                             });
+               /* *INDENT-ON* */
+
+               return offset - start;
+            }
+
+            //! \see HF::Protocol::Response::unpack.
+            size_t unpack (const Common::ByteArray &array, size_t offset = 0)
+            {
+               size_t start = offset;
+
+               offset += Protocol::Response::unpack(array, offset);
+
+               uint8_t count = 0;
+               offset += array.read(offset, count);
+
+               entries.clear();
+
+               _Entry entry;
+               for (uint8_t i = 0; i < count; i++)
+               {
+                  offset += entry.unpack(array, offset);
+                  entries.push_back(entry);
+               }
+
+               return offset - start;
+            }
+         };
+
+         template<>
+         struct GetEntriesResponse<void>: public Protocol::Response
+         {
+            uint8_t count;
+
+            GetEntriesResponse() : count(0)
+            {}
+
+            //! \see HF::Protocol::Response::size.
+            size_t size () const
+            {
+               size_t result = Protocol::Response::size() + sizeof(uint8_t);
+               return result;
+            }
+
+            //! \see HF::Protocol::Response::pack.
+            size_t pack (Common::ByteArray &array, size_t offset = 0) const
+            {
+               size_t start = offset;
+
+               offset += Protocol::Response::pack(array, offset);
+
+               uint8_t count = 0;
+               offset += array.write(offset, count);
+
+               return offset - start;
+            }
+
+            //! \see HF::Protocol::Response::unpack.
+            size_t unpack (const Common::ByteArray &array, size_t offset = 0)
+            {
+               size_t start = offset;
+
+               offset += Protocol::Response::unpack(array, offset);
+
+               uint8_t count = 0;
+               offset += array.read(offset, count);
+
+               return offset - start;
+            }
+         };
+
+         typedef GetEntriesResponse<void> GetEntriesEmptyResponse;
+
+         // =============================================================================
+         // API
+         // =============================================================================
+
+         /*!
+          * Top-level parent for session management functionality.
+          */
+         class AbstractServer
+         {
+            protected:
+
+            struct Session
+            {
+               uint16_t address;
+               bool valid;
+
+               Session (uint16_t _address = HF::Protocol::BROADCAST_ADDR, bool _valid = false):
+                  address(_address), valid(_valid)
+               {}
+            };
+
+            typedef std::vector<Session>        Container;
+            typedef Container::iterator         iterator;
+            typedef Container::const_iterator   const_iterator;
+
+            //! Session's database.
+            Container sessions;
+
+            // =============================================================================
+            // API
+            // =============================================================================
+
+            public:
+
+            void start_session (uint16_t address)
+            {
+               auto it = find (address);
+
+               if (it == sessions.end())  // Session does not exist.
+               {
+                  sessions.push_back(Session(address, true));
+               }
+               else  // Session already exists. Update state.
+               {
+                  it->valid = true;
+               }
+            }
+
+            void end_session (uint16_t address)
+            {
+               auto it = find(address);
+               if (it != sessions.end())
+               {
+                  sessions.erase(it);
+               }
+            }
+
+            bool exists (uint16_t address) const
+            {
+               return find(address) != sessions.end();
+            }
+
+            bool is_valid (uint16_t address) const
+            {
+               auto it = find(address);
+               if (it != sessions.end())
+               {
+                  return it->valid;
+               }
+               return false;
+            }
+
+            void invalidate()
+            {
+               std::for_each(sessions.begin(), sessions.end(), [](Session &session){
+                  session.valid = false;
+               });
+            }
+
+            protected:
+
+            Common::Result handle_command (CMD cmd, Protocol::Packet &packet, Common::ByteArray &payload,
+                                            size_t offset = 0);
+
+            iterator find(uint16_t address)
+            {
+               return std::find_if (sessions.begin (), sessions.end (),
+                                     [address](const Session &session)
+               {
+                  return address == session.address;
+               });
+            }
+
+            const_iterator find(uint16_t address) const
+            {
+               return std::find_if (sessions.cbegin (), sessions.cend (),
+                                     [address](const Session &session)
+               {
+                  return address == session.address;
+               });
+            }
+
+            size_t payload_size (CMD cmd) const;
+
+            Common::Result check_offset (uint16_t offset, uint8_t &count, uint16_t size) const;
+
+            virtual void send (const Protocol::Address &addr, Protocol::Message &message) = 0;
+
+            virtual uint16_t entries_size() const = 0;
+
+            virtual Common::Result entries(uint16_t offset, uint8_t count,
+                                             Common::ByteArray &payload) = 0;
+
+            Common::Result check_session (uint16_t address, Common::ByteArray &payload) const;
+         };
+
+         /*!
+          * Wrapper over persistent storage APIs to invalidate sessions on
+          * save and destroy.
+          */
+         template<typename Parent>
+         class Entries:public Parent
+         {
+            AbstractServer &session_mgr;
+
+            public:
+
+            typedef typename Parent::value_type  value_type;
+
+            Entries(AbstractServer &_session_mgr):Parent(),
+                  session_mgr(_session_mgr)
+            {}
+
+            template<typename... Args>
+            Common::Result save (Args... _args)
+            {
+               auto res = Parent::save(_args...);
+               session_mgr.invalidate();
+               return res;
+            }
+
+            template<typename... Args>
+            Common::Result destroy (Args... _args)
+            {
+               auto res = Parent::destroy(_args...);
+               session_mgr.invalidate();
+               return res;
+            }
+         };
+
+         /*!
+          * Helper template to inject session management functionality into
+          * services requiring it.
+          */
+         template<typename _Entries>
+         struct Server:public AbstractServer
+         {
+            typedef Entries<_Entries> Container;
+
+            Server():_entries(Entries<_Entries>(*this))
+            {}
+
+            Container &entries () const
+            {
+               return const_cast<Container &>(_entries);
+            }
+
+            protected:
+
+            uint16_t entries_size() const
+            {
+               return static_cast<uint16_t>(_entries.size());
+            }
+
+            typedef typename _Entries::value_type value_type;
+
+            Common::Result entries (uint16_t offset, uint8_t count,
+                                     Common::ByteArray &payload)
+            {
+               GetEntriesResponse<value_type> response;
+
+               response.code = check_offset (offset, count, entries_size());
+               if (response.code == Common::Result::OK)
+               {
+                  auto start = _entries.begin();
+                  std::advance(start, offset);
+
+                  auto end = start;
+                  std::advance(end, count);
+                  std::copy (start, end, std::back_inserter(response.entries));
+               }
+
+               payload = Common::ByteArray(response.size());
+
+               response.pack (payload);
+
+               return response.code;
+            }
+
+            private:
+
+            Container _entries;
+         };
+
+      }  // namespace SessionManagement
+
+   }  // namespace Core
+
+}  // namespace HF
+
+#endif /* HF_SESSION_MANAGEMENT_H */
---
+++ src/core/session_management_server.cpp
@@ -0,0 +1,169 @@
+// =============================================================================
+/*!
+ * \file       src/core/session_manangement_server.cpp
+ *
+ * This file contains the implementation of the session management functionality
+ * for the server side.
+ *
+ * \version    x.x.x
+ *
+ * \copyright  Copyright &copy; &nbsp; 2014 ULE Alliance
+ *
+ * For licensing information, please see the file 'LICENSE' in the root folder.
+ *
+ * Initial development by Bithium S.A. [http://www.bithium.com]
+ */
+// =============================================================================
+
+#include "hanfun/core/session_management.h"
+
+using namespace HF;
+using namespace HF::Common;
+using namespace HF::Core::SessionManagement;
+
+// =============================================================================
+// Defines
+// =============================================================================
+
+// =============================================================================
+// API
+// =============================================================================
+
+// =============================================================================
+// Server::handle
+// =============================================================================
+/*!
+ *
+ */
+// =============================================================================
+Common::Result AbstractServer::handle_command (CMD cmd, Protocol::Packet &packet,
+                                                  Common::ByteArray &payload, size_t offset)
+{
+   // Don't process group address.
+   if (packet.source.mod == HF::Protocol::Address::GROUP)
+   {
+      return Common::Result::FAIL_ARG;
+   }
+
+   Result result = Result::OK;
+
+   Protocol::Message resp_msg (packet.message);
+
+   switch (cmd)
+   {
+      case START:
+      {
+         start_session(packet.source.device);
+
+         StartResponse resp;
+         result = resp.code = Result::OK;
+         resp.count = entries_size();
+
+         resp_msg.payload = Common::ByteArray(resp.size());
+         resp.pack(resp_msg.payload);
+
+         break;
+      }
+      case GET:
+      {
+         result = check_session(packet.source.device, resp_msg.payload);
+         if (result == Result::OK)
+         {
+            GetEntriesMessage msg;
+            msg.unpack(payload, offset);
+            result = entries(msg.offset, msg.count, resp_msg.payload);
+         }
+         break;
+      }
+      case END:
+      {
+         end_session(packet.source.device);
+
+         Protocol::Response resp;
+         result = resp.code = Result::OK;
+         resp_msg.payload = Common::ByteArray (resp.size());
+         resp.pack(resp_msg.payload);
+
+         break;
+      }
+      default:
+         return Result::FAIL_SUPPORT;
+   }
+
+   send(packet.source, resp_msg);
+
+   return result;
+}
+
+// =============================================================================
+// AbstractServer::check_session
+// =============================================================================
+/*!
+ *
+ */
+// =============================================================================
+Common::Result AbstractServer::check_session (uint16_t address, Common::ByteArray &payload) const
+{
+   Result result = Result::OK;
+
+   if (!exists(address))
+   {
+      result = Result::FAIL_READ_SESSION;
+   }
+   else if (!is_valid(address))
+   {
+      result = Result::FAIL_MODIFIED;
+   }
+
+   if (result != Result::OK)
+   {
+      GetEntriesEmptyResponse resp;
+      resp.code = result;
+      payload = Common::ByteArray(resp.size());
+      resp.pack(payload);
+   }
+
+   return result;
+}
+
+// =============================================================================
+// check_offset
+// =============================================================================
+/*!
+ *
+ */
+// =============================================================================
+Common::Result AbstractServer::check_offset (uint16_t offset, uint8_t &count, uint16_t size) const
+{
+   if (offset >= size)
+   {
+      return Common::Result::FAIL_ARG;
+   }
+   else if (offset + count >= size)
+   {
+      count = size - offset;
+   }
+
+   return Common::Result::OK;
+}
+
+// =============================================================================
+// AbstractServer::payload_size
+// =============================================================================
+/*!
+ *
+ */
+// =============================================================================
+size_t AbstractServer::payload_size (CMD cmd) const
+{
+   switch (cmd)
+   {
+      case GET:
+         return GetEntriesMessage::min_size;
+      case START:
+      case END:
+      default:
+         return 0;
+      break;
+   }
+}
