f108dfdb3e2b384a7bd162da0fff57d639627b76
Filipe Alves <filipe.alves@bithium.com>
2015-11-17 18:15:45 +0000

    Added attribute code generation support.

--- tools/hanfun.rb
+++ tools/hanfun.rb
@@ -28,11 +28,18 @@ module Hanfun
   TemplateRoot = File.join(HAN_FUN_ROOT, 'hanfun')
   CodeRoot     = File.expand_path(File.join(HAN_FUN_ROOT, '..'))
 
+  CHeader = %q{
+
+// =============================================================================
+// <header>
+// =============================================================================
+
+}
   # =============================================================================
   # Helper classes
   # =============================================================================
 
-  class Interface
+  class Option
     attr_reader :name
     attr_reader :to_doc
     attr_reader :to_class
@@ -46,6 +53,39 @@ module Hanfun
     end
   end
 
+  class Interface < Option
+
+    def initialize(name)
+      super
+    end
+
+  end
+
+  class Attribute < Option
+
+    attr_reader :uid
+    attr_reader :type
+    attr_reader :writable
+    attr_reader :mandatory
+
+    def initialize(name, interface, options)
+      super(name)
+      @to_uid = @name.upcase + "_ATTR"
+      @interface = interface
+      if options =~ /(\w+):(\d+):(rw?):(m|o)/i
+        @type      = $1
+        @uid       = $2.to_i
+        @writable  = $3
+        @mandatory = $4
+
+        @writable  = @writable =~ /w/i ? true : false
+        @mandatory  = @mandatory =~ /m/i ? true : false
+      else
+        raise ArgumentError, "Invalid option string '#{options}'"
+      end
+    end
+
+  end
   module Generators
 
     # =============================================================================
@@ -69,6 +109,10 @@ module Hanfun
         @namespace = namespace
       end
 
+      def top_path(name)
+        File.join(CodeRoot, name)
+      end
+
       def include_path(name, append = true)
         file = File.join(CodeRoot, "inc", "hanfun")
         file = File.join(file, @namespace) if append
@@ -86,19 +130,51 @@ module Hanfun
         file = File.join(file, @namespace) if append
         return File.join(file, name)
       end
+
+      def cmake_path(name)
+        File.join(CodeRoot, "cmake", name)
+      end
     end
 
     class Interface < Abstract
 
+      class_option "attributes", desc: "Attributes for the interface", aliases: "-a", type: :hash
+
       def __initialize__
         super("interfaces")
         @interface = Hanfun::Interface.new(name)
+        @attributes = []
+        options[:attributes].each do |key,value|
+          @attributes << Attribute.new(key, @interface, value)
+        end
+        @attributes.sort! { |a,b| a.uid <=> b.uid }
       end
 
       desc "Add interface UID."
       def add_uid
         code = "#{@interface.to_uid} = #{uid},  //!< FIXME #{@interface.to_doc} interface UID.\n"
         inject_into_file include_path("interface.h", false), "         #{code}", :before => /^\s+\/\*\s+Reserved/
+      end
+
+      desc "Add configuration options"
+      def config_files
+
+        header = CHeader.gsub(/<header>/, "#{@interface.to_doc} Configuration")
+
+        code_opts = []
+        code_defs = []
+        @attributes.select { |attr| !attr.mandatory }.each do |attr|
+          name = "HF_ITF_#{@interface.to_uid}_#{attr.to_uid}"
+          code_opts << "option(#{name} \"Interface - #{@interface.to_doc} - #{attr.to_doc}\")"
+          code_defs << "#define #{name}  @#{name}@"
+        end
+
+        # Add options for options attributes to cmake/options.cmake
+        append_to_file cmake_path("options.cmake"), header.gsub(/^\/\//, "#") + code_opts.join("\n") + "\n"
+
+        # Add optional attributes to config.h.in
+        inject_into_file source_path("../config.h.in", false), header + code_defs.join('\n'),
+                         :before => /\s+#endif/
       end
 
       desc "Generate include file."
@@ -132,9 +208,20 @@ module Hanfun
         # Generate test code.
         template("interface/test.cpp.erb", test_path("test_#{name}.cpp"))
 
+        # Add optional attributes for test config.h.in
+        header = CHeader.gsub(/<header>/, "#{@interface.to_doc} Configuration")
+
+        code = []
+        @attributes.select { |attr| !attr.mandatory }.each do |attr|
+          code << "#define HF_ITF_#{@interface.to_uid}_#{attr.to_uid}  1"
+        end
+        inject_into_file test_path("config.h.in", false), header + code.join('\n'),
+                         :before => /\s+\/\/\s+=+\n\/\/\s+Other/
+
         # Add test code to file.
         code = "add_interface(\"#{name}\")\n"
-        inject_into_file test_path("CMakeLists.txt", false), code, :before => /^\s+##\s+Profiles/
+        inject_into_file test_path("CMakeLists.txt", false), code,
+                         :before => /^\s+##\s+Profiles/
       end
 
     end
