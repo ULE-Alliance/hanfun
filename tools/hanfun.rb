0332c91f9016b739138dbab9ceaa5d178df6f769
Filipe Alves <filipe.alves@bithium.com>
2015-11-18 17:23:28 +0000

    Initial support for generating commands for interfaces.

--- tools/hanfun.rb
+++ tools/hanfun.rb
@@ -90,6 +90,38 @@ module Hanfun
 
   end
 
+  class Command < Option
+
+    class Role
+      attr_reader :mandatory
+
+      def initialize(flag)
+        @mandatory = flag =~ /m/ ? true : false
+      end
+    end
+
+    attr_reader :uid
+    attr_reader :event
+    attr_reader :server
+    attr_reader :client
+
+    def initialize(name, options)
+      super(name)
+      @to_uid = @name.upcase + "_CMD"
+      if options =~ /(\d+):(c|e):(m|o):(m|o)/i
+        @uid       = $1.to_i
+        @event     = $2
+        @server    = Role.new($3)
+        @client    = Role.new($4)
+
+        @event     = @event =~ /e/i ? true : false
+      else
+        raise ArgumentError, "Invalid option string '#{options}'"
+      end
+    end
+
+  end
+
   module Generators
 
     # =============================================================================
@@ -144,15 +176,29 @@ module Hanfun
     class Interface < Abstract
 
       class_option "attributes", desc: "Attributes for the interface", aliases: "-a", type: :hash
+      class_option "commands", desc: "Commands for the interface", aliases: "-c", type: :hash
 
       def __initialize__
         super("interfaces")
         @interface = Hanfun::Interface.new(name, short)
+
+        # Handle attributes.
         @attributes = []
         options[:attributes].each do |key,value|
           @attributes << Attribute.new(key, value)
         end
         @attributes.sort! { |a,b| a.uid <=> b.uid }
+
+        # Handle commands
+        @commands = []
+        options[:commands].each do |key,value|
+          @commands << Command.new(key, value)
+        end
+        @commands.sort! { |a,b| a.uid <=> b.uid }
+
+        sizes = @commands.map { |c| c.to_uid.size }
+        sizes += @attributes.map { |a| a.to_uid.size }
+        @uid_align = sizes.max
       end
 
       desc "Add interface UID."
@@ -169,18 +215,31 @@ module Hanfun
 
         code_opts = []
         code_defs = []
+        @commands.select { |cmd| !cmd.server.mandatory || !cmd.client.mandatory}.each do |cmd|
+          name = "HF_ITF_#{@interface.short}_#{cmd.to_uid.ljust(@uid_align)}"
+          code_opts << "option(#{name} \"Interface - #{@interface.to_doc} - #{cmd.to_doc}\")"
+          code_defs << "#define #{name}  @#{name.strip}@"
+        end
+
+        # Add empty line between commands and attributes
+        code_opts << ""
+        code_defs << ""
+
         @attributes.select { |attr| !attr.mandatory }.each do |attr|
-          name = "HF_ITF_#{@interface.short}_#{attr.to_uid}"
+          name = "HF_ITF_#{@interface.short}_#{attr.to_uid.ljust(@uid_align)}"
           code_opts << "option(#{name} \"Interface - #{@interface.to_doc} - #{attr.to_doc}\")"
-          code_defs << "#define #{name}  @#{name}@"
+          code_defs << "#define #{name}  @#{name.strip}@"
         end
 
         # Add options for options attributes to cmake/options.cmake
-        append_to_file cmake_path("options.cmake"), header.gsub(/^\/\//, "#") + code_opts.join("\n") + "\n"
+        append_to_file cmake_path("options.cmake") do
+          header.gsub(/^\/\//, "#") + code_opts.join("\n") + "\n"
+        end
 
         # Add optional attributes to config.h.in
-        inject_into_file source_path("../config.h.in", false), header + code_defs.join('\n'),
-                         :before => /\s+#endif/
+        inject_into_file top_path("config.h.in"), :before => /\s+#endif/ do
+          header + code_defs.join("\n")
+        end
       end
 
       desc "Generate include file."
@@ -228,11 +287,20 @@ module Hanfun
         header = CHeader.gsub(/<header>/, "#{@interface.to_doc} Configuration")
 
         code = []
-        @attributes.select { |attr| !attr.mandatory }.each do |attr|
-          code << "#define HF_ITF_#{@interface.short}_#{attr.to_uid}  1"
+        @commands.select { |cmd| !cmd.server.mandatory || !cmd.client.mandatory }.each do |cmd|
+          code << "#define HF_ITF_#{@interface.short}_#{cmd.to_uid.ljust(@uid_align)}  1"
         end
-        inject_into_file test_path("config.h.in", false), header + code.join('\n'),
-                         :before => /\s+\/\/\s+=+\n\/\/\s+Other/
+
+        # Put empty line between commands and attributes
+        code << ""
+
+        @attributes.select { |attr| !attr.mandatory }.each do |attr|
+          code << "#define HF_ITF_#{@interface.short}_#{attr.to_uid.ljust(@uid_align)}  1"
+        end
+
+        inject_into_file test_path("config.h.in", false), :before => /\s+\/\/\s+=+\n\/\/\s+Other/ do
+          header + code.join("\n")
+        end
 
         # Add test code to file.
         code = "add_interface(\"#{name}\")\n"
