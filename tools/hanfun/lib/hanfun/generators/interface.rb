09d45172758c91f2f1269af4a77221f8c478b1e9
Filipe Alves <filipe.alves@bithium.com>
2016-05-06 18:53:57 +0100

    CHG: Helper generator tool refactor.
    Refactor helper generator tool into separated files.

---
+++ tools/hanfun/lib/hanfun/generators/interface.rb
@@ -0,0 +1,77 @@
+# =============================================================================
+#
+# @file       tools/hanfun/lib/generators/interface.rb
+#
+# This file contains the implementation for the the interfaces generator.
+#
+# @version    x.x.x
+#
+# @copyright  Copyright &copy; &nbsp; 2016 ULE Alliance
+#
+# For licensing information, please see the file 'LICENSE' in the root folder.
+#
+# Initial development by Bithium S.A. [http://www.bithium.com]
+#
+# =============================================================================
+
+require_relative 'abstract'
+
+module Hanfun
+
+  module Generators
+
+    #
+    # This class is responsible for the generation of new HAN-FUN interfaces.
+    #
+    class Interface < Abstract
+
+      desc "Generator for new HAN-FUN interfaces."
+
+      def configure
+        @type = "interface"
+        @namespace = Hanfun::Namespace.new("Interfaces", "ITF")
+
+        @generator = {}
+        @generator[:uid] = {
+          reference: /\bALERT\b/,
+          insert_at: /^\s+\/\*\s+Reserved/,
+        }
+
+        @generator[:config] = {
+          insert_config_at: /\s#endif/,
+          insert_cmake_at: /^\s+#\s+=+\n#\s+Dependecies/
+        }
+
+        @generator[:debug] = {
+          insert_at: /^\s+\/\/\s+=+\n\/\/\s+Core/,
+          protocol: "Unknown",
+        }
+
+        @generator[:include] = {
+          insert_at: /^\s+\/\/\s+=+\n\/\/\s+Core/,
+        }
+
+        @generator[:attributes] = {
+          insert_at: {
+          include: /^\s.+core/,
+          factory: /};\s+\/\/\s+=+\s+\/\/\s+Attributes::get_factory/
+          }
+        }
+
+        @generator[:build] = {
+          insert_at: /^\s+##\s+Core/,
+          macro: "add_interface"
+        }
+
+        @generator[:tests] = {
+          insert_config_at: /\s+\/\/\s+=+\n\/\/\s+Other/,
+          insert_cmake_at: /^\s+##\s+Profiles/,
+        }
+        super
+      end
+
+    end
+
+  end # Hanfun::Generator
+
+end # Hanfun
---
+++ tools/hanfun/lib/hanfun/helpers.rb
@@ -0,0 +1,149 @@
+# =============================================================================
+#
+# @file       tools/hanfun/lib/hanfun.rb
+#
+# This file contains the implementation of helper classes used by the hanfun
+# generators.
+#
+# @version    x.x.x
+#
+# @copyright  Copyright &copy; &nbsp; 2016 ULE Alliance
+#
+# For licensing information, please see the file 'LICENSE' in the root folder.
+#
+# Initial development by Bithium S.A. [http://www.bithium.com]
+#
+# =============================================================================
+
+# =============================================================================
+# Helper classes
+# =============================================================================
+
+module Hanfun
+
+  Root = File.expand_path(File.join(File.dirname(__dir__), '..'))
+
+  TemplateRoot = File.join(Root, 'templates')
+  CodeRoot     = File.expand_path(File.join(Root, '../..'))
+
+  CHeader = %q{
+// =============================================================================
+// <header>
+// =============================================================================
+
+}
+
+  # Base class for all the generator options
+  class Option
+    attr_reader :name
+    attr_reader :to_doc
+    attr_reader :to_class
+    attr_reader :to_uid
+
+    def initialize(name)
+      @name     = name.underscore
+      @to_doc   = @name.human_name
+      @to_class = @name.camelize
+      @to_uid   = @name.upcase
+    end
+  end
+
+  # Interface configuration
+  class Interface < Option
+
+    attr_reader :short
+    attr_reader :path
+
+    def initialize(name, short)
+      super(name)
+      @name = @to_class = @to_doc = name if name =~ /\A[A-Z]+\z/
+      @short = short
+      @short = @to_uid unless short
+      @path  = @name.underscore
+    end
+
+  end
+
+  # Interface/Service attribute configuration
+  class Attribute < Option
+
+    attr_reader :uid
+    attr_reader :type
+    attr_reader :writable
+    attr_reader :mandatory
+    def initialize(name, options)
+      super(name)
+      @to_uid = @name.upcase + "_ATTR"
+      if options =~ /(\w+):(\d+):(rw?):(m|o)(:(\w+))?/i
+        @type         = $1
+        @uid          = $2.to_i
+        @writable     = $3
+        @mandatory    = $4
+        name_override = $6
+
+        @writable  = @writable =~ /w/i ? true : false
+        @mandatory = @mandatory =~ /m/i ? true : false
+
+        @name     = name_override if name_override
+        @to_class = @name.camelize if name_override
+
+        @to_doc   = name if name =~ /\A[A-Z]+\z/
+      else
+        raise ArgumentError, "Invalid option string '#{options}'"
+      end
+    end
+
+  end
+
+  # Interface/Service command configuration
+  class Command < Option
+
+    class Role
+      attr_reader :mandatory
+
+      def initialize(flag)
+        @mandatory = flag =~ /^m.*/i ? true : false
+      end
+    end
+
+    attr_reader :uid
+    attr_reader :event
+    attr_reader :server
+    attr_reader :client
+
+    def initialize(name, options)
+      super(name)
+      @to_uid = @name.upcase + "_CMD"
+      if options =~ /(\d+):(c|e):(m|o):(m|o)/i
+        @uid       = $1.to_i
+        @event     = $2
+        @server    = Role.new($3)
+        @client    = Role.new($4)
+
+        @event     = @event =~ /e/i ? true : false
+      else
+        raise ArgumentError, "Invalid option string '#{options}'"
+      end
+
+    end
+
+  end
+
+  # Interface/Service namespace configuration
+  class Namespace < Option
+
+    attr_reader :path
+    attr_reader :short
+    attr_reader :include
+
+    def initialize(name, short)
+      @name    = name
+      @short   = short
+      @short   = @to_uid unless short
+
+      @path    = name.downcase
+      @include = @path.gsub(/s$/, '')
+    end
+  end
+
+end # Hanfun
